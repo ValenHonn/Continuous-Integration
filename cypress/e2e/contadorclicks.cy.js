7b36c0ced8986a8a1c9a27bef01acbb58883838b
Valen <valentinohonnorat2@gmail.com>
2025-05-26 10:45:28 -0300

    comandos arreglados en el test de cypress

--- cypress/e2e/contadorclicks.cy.js
+++ cypress/e2e/contadorclicks.cy.js
@@ -1,12 +1,12 @@
 describe('Contador de Clicks', () => {
-  beforeEach(() => {
+  beforeEach(() => { //antes de cada it, visita la pagina
     cy.visit('http://localhost:5173'); 
   });
 
-  it('debe incrementar el contador al hacer clic', () => {
-    cy.contains('Clicks: 0');
+  it('debe incrementar el contador al hacer click', () => { 
+    cy.contains('Clicks: 0'); //busca el elemento con ese texto
+    cy.get('#botonClick').click(); //busca el elemento con ese id, simula un click
     cy.get('#botonClick').click();
-    cy.get('#botonClick').click();
-    cy.contains('Clicks: 2');
+    cy.contains('Clicks: 2'); //verifica que haya un elemento con el texto Clicks: 2
   });
 });
