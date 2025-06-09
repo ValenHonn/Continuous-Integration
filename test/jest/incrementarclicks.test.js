2796ef20dee629b321ed1af8d5ca35e9b1786fef
valen <valentinohonnorat2@gmail.com>
2025-06-09 19:07:33 -0300

    prueba error del test unitario de la rama nueva

--- test/jest/incrementarclicks.test.js
+++ test/jest/incrementarclicks.test.js
@@ -18,7 +18,7 @@ describe('Pruebas para la función incrementarClick', () => {
   }); 
 
   test('Debería incrementar el contador correctamente', () => {
-    expect(incrementarClick()).toBe(1);  
+    expect(incrementarClick()).toBe(2);  
     expect(incrementarClick()).toBe(2); 
     expect(incrementarClick()).toBe(3);  
   });
